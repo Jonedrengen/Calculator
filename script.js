4a6e757f8a52895c15222ee51b6e92ab81fa3a7e
Jon Slotved <jonsslotved@gmail.com>
2024-02-12 11:50:55 +0100

    Add inputNumber variable and console.log statement

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // FILEPATH: /C:/Users/Ole_S/OneDrive/Dokumenter/GitHub/Calculator/script.js
 
 
+// get input values
+var inputNumber = document.getElementById('inputNumber');
+console.log(inputNumber.value);
+
+inputField =
+
 // 1. Create a function that takes two numbers and returns the sum of those numbers
 function add(a, b) {
     return a + b;
