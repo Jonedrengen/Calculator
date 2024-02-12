348604a549ff7d9307c9e8c02def247993b7cea4
Jon Slotved <jonsslotved@gmail.com>
2024-02-12 20:17:30 +0100

    added an eventlistener

--- script.js
+++ script.js
@@ -4,25 +4,27 @@
 // the following is code to make the buttons add their numbers to the input field
 // the following is code to make the buttons add their numbers to the input field
 
-// getting the input element
-var inputField = document.getElementById("inputNumber"); 
-console.log(inputField);
-// getting all the buttons
-var buttons = document.getElementsByClassName("genericButton")
-console.log(buttons.length);
+// adding an event listener to the document, which will run the function when the document is loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    // getting the input element
+    var inputField = document.getElementById("inputNumber"); 
+    console.log(inputField);
+    // getting all the buttons
+    var buttons = document.getElementsByClassName("genericButton")
+    console.log(buttons.length);
 
-// function that add numbers from buttons to the input field
-function addNumberToInput(num) {
-    inputField.value = (Number(inputField.value) + num).toSting();
-    console.log(typeof inputField);
-}
+    // function that add numbers from buttons to the input field
+    function addNumberToInput(num) {
+        inputField.value = (Number(inputField.value) + num).toString();
+        console.log(typeof inputField);
+    }
 
-// adding a listener to each button
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function() {
-        // Call the function to add the number to the input field
-        addNumberToInput(Number(this.value));
-    });
-}
-
+    // adding a listener to each button
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            // Call the function to add the number to the input field
+            addNumberToInput(Number(this.value));
+        });
+    }
+});
 
