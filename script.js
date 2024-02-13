50bf00d6a662e59a8cb720310684c8aaca4ba553
Jon Slotved <127400053+Jonedrengen@users.noreply.github.com>
2024-02-13 13:48:33 +0100

    Refactor button event listeners and add comments

--- script.js
+++ script.js
@@ -4,26 +4,25 @@
 // the following is code to make the buttons add their numbers to the input field
 // the following is code to make the buttons add their numbers to the input field
 
-// adding an event listener to the document, which will run the function when the document is loaded
+// adding an event listener to the document, which means that functions will be called when the document is fully loaded and not before
 document.addEventListener('DOMContentLoaded', (event) => {
     // getting the input element
     var inputField = document.getElementById("inputNumber"); 
-    console.log(inputField);
     // getting all the buttons
-    var buttons = document.getElementsByClassName("genericButton")
-    console.log(buttons.length);
-
+    var numberButtons = document.getElementsByClassName("genericButton number");
+    console.table({ 'array buttons length': numberButtons.length, 'buttons type': typeof numberButtons });
+    console.table({ 'events': event });
     // function that add numbers from buttons to the input field
     function addNumberToInput(num) {
-        inputField.value = (Number(inputField.value) + num).toString();
-        console.log(typeof inputField);
+        inputField.value = 0; // reset the input field to 0
+        inputField.value = (Number(inputField.value) + num).toString(); // add the number from the button to the input field
     }
 
     // adding a listener to each button
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function() {
-            // Call the function to add the number to the input field
-            addNumberToInput(Number(this.value));
+    for (var i = 0; i < numberButtons.length; i++) { // loop through all the buttons, start from 0 and end at the length of the buttons (i < buttons.length) (i++ means increment by 1)
+        numberButtons[i].addEventListener('click', function() { // add an event listener to each button, where the event is a click
+            addNumberToInput(Number(this.value)); // every click will call the addNumberToInput function
+            console.log('value of button added = ' + this.value);
         });
     }
 });
